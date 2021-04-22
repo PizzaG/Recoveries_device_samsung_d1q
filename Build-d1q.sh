3d4e21c2a6de803b3acc0224bae613cb39e8f13a
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-22 16:51:56 -0500

    Add files via upload

---
+++ Build-d1q.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Nusantara
+#############################
+make clean
+ccache -M 50G
+export USE_CCACHE=1
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL=C
+export NR_MAINTAINER=PizzaG
+. build/envsetup.sh
+lunch omni_d1q-eng
+mka recoveryimage -j$(nproc --all)
+echo
+echo " Recovery Should Be Built"
+echo ""
+mv $OUT/recovery.img /media/pizzag/Android/Uploads/Recoveries/Nusantara_Recovery-d1q.img
+mv $OUT/NR-d1q*.zip /media/pizzag/Android/Uploads/Recoveries/Nusantara_Recovery-d1q.zip
+mv $OUT/recovery.tar /media/pizzag/Android/Uploads/Recoveries/Nusantara_Recovery-d1q.tar
+cd /media/pizzag/Android/Recoveries/Nusantara/8
+make clean
+#############################
+#############################
+
