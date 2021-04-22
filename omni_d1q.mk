16fff83eea43c97f1fd7a5b5b36fed48a06d122b
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-04-22 16:49:36 -0500

    Add files via upload

---
+++ omni_d1q.mk
@@ -0,0 +1,28 @@
+# Release name
+PRODUCT_RELEASE_NAME := d1q
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/batik/config/common.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/d1q/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_d1q
+PRODUCT_DEVICE := d1q
+PRODUCT_MODEL := SM-N9700
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
